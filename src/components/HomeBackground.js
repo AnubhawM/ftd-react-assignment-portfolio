328c9ba58d7d3919ef77c0a3cad738a209357b40
AnubhawM <anubo16@gmail.com>
2019-10-03 09:30:31 -0500

    Added new background to Home page

--- src/components/HomeBackground.js
+++ src/components/HomeBackground.js
@@ -2,7 +2,10 @@ import styled from 'styled-components'
 
 const HomeBackground = styled.div`
   border: 1px solid #000;
-  background-image: url('https://images.pexels.com/photos/1169754/pexels-photo-1169754.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260');
+  // background-image: url('https://images.pexels.com/photos/1169754/pexels-photo-1169754.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260');
+  background-image: url('https://i.pinimg.com/originals/00/2d/7e/002d7e638fb463fb7a266f5ffc7ac47d.gif');
+  background-repeat: no-repeat;
+
   width: 100vw;
   height: 100vw;
 `
